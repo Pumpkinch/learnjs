6d84eb91eb35c848bd38023a701fe120fc0d62cf
Pumpkinch <melkiyfufonchik@gmail.com>
2023-06-08 19:31:07 +0300

    comparison

---
+++ part_1/comparison/comparison.js
@@ -0,0 +1,8 @@
+// https://learn.javascript.ru/comparison#tasks
+5 > 4 //true
+"ананас" > "яблоко" //false
+"2" > "12"  //true
+undefined == null //true
+undefined === null //false
+null == "\n0\n" //false
+null === +"\n0\n"  //false
