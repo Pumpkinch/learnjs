11cbb3333b5026cfe8256efc97cd23f7cece8b2e
Pumpkinch <melkiyfufonchik@gmail.com>
2023-06-10 15:38:44 +0300

    while-for2

--- part_1/while-for/seventh.js
+++ part_1/while-for/seventh.js
@@ -1,8 +1,8 @@
 // https://learn.javascript.ru/while-for#vyvesti-prostye-chisla
 let a = prompt('Введите a', '');
-stop:
-for(let b=2; b <= a; b++){
-    for(let c=2; c < b; c++){
-    if(b % c === 0) continue stop;
-    } alert(b);
+for (let b = 2; b <= a; b++) {
+    for (let c = 2; c < b; c++) {
+        if (b % c === 0) break; // прерываем этот цикл, он нам больше неинтересен
+        if (c === b - 1) alert(b); // дошли до конца цикла, значит ничего не делилось, это наше число
+    }
 }
