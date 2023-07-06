dda0783619d3e115fc60794b9e306b7854358f9b
Pumpkinch <melkiyfufonchik@gmail.com>
2023-07-06 11:37:04 +0300

    keys-values-entries

---
+++ part_1/keys-values-entries/second.js
@@ -0,0 +1,11 @@
+// https://learn.javascript.ru/keys-values-entries#podschyot-kolichestva-svoystv-obekta
+let user = {
+    name: 'John',
+    age: 30
+};
+
+const count = (obj) => {
+    return lot = (Object.keys(obj)).length;
+}
+
+alert(count(user));
