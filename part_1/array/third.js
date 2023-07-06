e36ca90b41bc8b3edecfe44b89a363f03d995414
Pumpkinch <melkiyfufonchik@gmail.com>
2023-07-06 13:17:16 +0300

    array-finish

--- part_1/array/third.js
+++ part_1/array/third.js
@@ -1,11 +1,15 @@
 // https://learn.javascript.ru/array#summa-vvedyonnyh-chisel
 const sumInput = () => {
     let arr = [];
+    let isFinished = true;
 
-    while (isFinite) {
+    while (isFinished) {
         let item = prompt('Введите число', 0);
-        if (item === null || !isFinite(item)) break;
+        if (item === null || item === '' || !isFinite(item)) break;
         arr.push(+item);
+        if (arr > 10) {
+            isFinished = false;
+        }
     }
 
     let sum = 0;
