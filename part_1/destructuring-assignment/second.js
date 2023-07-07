fa80598a850b3f3bad7a2053bb74e6269e70b97f
Pumpkinch <melkiyfufonchik@gmail.com>
2023-07-07 11:37:45 +0300

    destructuring-finish

--- part_1/destructuring-assignment/second.js
+++ part_1/destructuring-assignment/second.js
@@ -5,17 +5,20 @@ let salaries = {
     "Mary": 250
 };
 
-const topSalary = (users) => {
+const topSalary = (localSalaries) => {
     let maxSalary = 0;
     let employee = null;
 
-    for (let [key, value] of Object.entries(users)) {
-        if (maxSalary <= value) {
-            maxSalary = value;
-            employee = key;
+    if (!localSalaries) {
+        return 'localSalaries not find';
+    } else {
+        for (let [key, value] of Object.entries(localSalaries)) {
+            if (maxSalary <= value) {
+                maxSalary = value;
+                employee = key;
+            }
         }
+        return employee;
     }
-    return employee;
-
 }
 alert(topSalary(salaries));
